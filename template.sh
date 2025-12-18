8f826dbb63e1508489b5f2c2cb840be14eee8b37
klxcoder <klxcoder@gmail.com>
2025-12-18 10:02:52 +0700

    mkdir and touch

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Create folders
+mkdir -p src
+mkdir -p research
+
+# Create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirments.txt
+
+echo "Folders and Files created successfully!"
